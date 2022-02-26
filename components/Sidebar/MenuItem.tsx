7b8c7fb54742e5ba45e3d2cc9d0c6c5d68f9b694
kubo-hide-kun <hide44041@gmail.com>
2022-02-26 16:24:45 +0900

    style: remove illegal white space

--- components/Sidebar/MenuItem.tsx
+++ components/Sidebar/MenuItem.tsx
@@ -13,7 +13,7 @@ export const MenuItem: React.FC<Props> = ({ label, icon, active, onClick }) => {
   return (
     <li
       className={classNames(
-        'cursor-pointer select-none list-none rounded-lg px-4 py-2	text-primary-regular',
+        'cursor-pointer select-none list-none rounded-lg px-4 py-2 text-primary-regular',
         {
           'bg-accent-primary-regular text-primary-inverted shadow-md': active,
           'hover:bg-secondary-regular': !active,
