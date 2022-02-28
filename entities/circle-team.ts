2adc868affe4281a6ce6c36507bb23a7abb02e1f
kubo-hide-kun <hide44041@gmail.com>
2022-02-28 11:41:28 +0900

    feat: add MeetUp property on circle team entity

--- entities/circle-team.ts
+++ entities/circle-team.ts
@@ -3,6 +3,8 @@ export type CircleTeam = {
   circleId: string;
   title: string;
   description: string;
+  shouldMeetUp: boolean; // 交流イベントの開催が必須か否か
+  MeetUpIntervalDay: number; // 交流イベントの頻度（日）
   createdAt: Date;
   updatedAt: Date;
   createdBy: string;
@@ -27,3 +29,19 @@ export const MEMBER_ROLE = {
   MEMBER: 'member',
 } as const;
 export type MemberRole = typeof MEMBER_ROLE[keyof typeof MEMBER_ROLE];
+
+// 交流イベント
+export type CircleTeamMeetUP = {
+  interviewId: string;
+  relationId: string;
+  circleId: string;
+  title: string;
+  note: string;
+  openedAt: Date; // 交流イベントの実施日
+  announced: boolean; // 参加者に交流イベントの開催をアナウンスしたか否か
+  completed: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+  createdBy: string;
+  updatedBy: string;
+};
